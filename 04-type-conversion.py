f4b6cdfa80cfcedf3745e462d24648fd4d510851
Shankar-codes <harikainteriors@gmail.com>
2026-07-18 16:30:00 +0530

    python notes

---
+++ 04-type-conversion.py
@@ -0,0 +1,9 @@
+x = "10"
+print(x)
+y = int(x)
+print(int(y))
+z= float(y)
+print(int(z))
+value=12.345
+formatted_value=f"{value:.2f}" # :.2f means 2 decimal points if :.3f means 3 decimal points
+print(formatted_value)
