ac36f50b5ed1190af5d5ce692ad86b90e5522820
Shankar-codes <harikainteriors@gmail.com>
2026-07-18 17:46:23 +0530

    python notes

--- 17-logical-operators.py
+++ 17-logical-operators.py
@@ -2,6 +2,6 @@ x = 5
 y = 10
 z = 15
 
-print(x > 0 and y > 5)
-print(x > 10 or z > 10)
-print(not(x > 10))
+print(x > 0 and y > 5) # both conditions are right so true
+print(x > 10 or z > 25) # both conditions are wrong so false
+print(not(x > 10)) # the conditon is wrong so displays the output True
