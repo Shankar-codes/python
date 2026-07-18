386e124c145d071ad79eee3ebdcbfc7d8199e91e
Shankar-codes <harikainteriors@gmail.com>
2026-07-18 17:37:54 +0530

    python notes

---
+++ 16-operators.py
@@ -0,0 +1,9 @@
+a = 10
+b = 20
+
+print(a==b) # equal to
+print(a!=b) # not equal to
+print(a>b) # greater than
+print(a<b) # lesser than
+print(a>=10) # greater than or equal to 
+print(b<=25) # lesser than or equal to
