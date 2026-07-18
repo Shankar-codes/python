309a5259f356af2477f84ef67fd459f654204e16
Shankar-codes <harikainteriors@gmail.com>
2026-07-18 17:05:24 +0530

    python notes

---
+++ 11-string-manipulation.py
@@ -0,0 +1,9 @@
+# string declaration
+first_name = "Shankar"
+second_name = "Ellamma"
+
+print("first name :", first_name)
+print("second name :", second_name)
+# string concatenation
+full_name = first_name + " " + second_name
+print("Full name = ", full_name)
