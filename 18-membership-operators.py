d14dfe24371b29a732e22e77bc4347cc57a70c36
Shankar-codes <harikainteriors@gmail.com>
2026-07-18 17:57:10 +0530

    python notes

---
+++ 18-membership-operators.py
@@ -0,0 +1,6 @@
+# "in" and "not in" are membership operators
+DevOps = ["Linux", "Shell", "Ansible", "Terraform", "Docker", "Kubernetes", "Python"]
+
+
+print("Linux" in DevOps)
+print("Shell" not in DevOps)
