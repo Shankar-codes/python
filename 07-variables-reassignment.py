18b28b697fc147cf09efd520cc18b2f04a69e7cf
Shankar-codes <harikainteriors@gmail.com>
2026-07-18 16:43:35 +0530

    python notes

---
+++ 07-variables-reassignment.py
@@ -0,0 +1,26 @@
+x = 5
+print(x)
+x = 10
+print(x)
+
+#variables
+x = 5
+y = 10
+name = "Shankar"
+z = 3.14
+is_indian = True
+
+print(type(x))
+print(type(y))
+print(type(name))
+print(type(z))
+print(type(is_indian))
+
+x = None
+print(type(x))
+
+name = "Shankar"
+age = "25"
+
+# type conversion examples
+print(f"My name is {name} and my age is {int(age)}. I am indian")
