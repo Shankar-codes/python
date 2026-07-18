5413263ca6330fdfbfde665fe19cf31284231983
Shankar-codes <harikainteriors@gmail.com>
2026-07-18 18:24:16 +0530

    python notes

--- 19-list.py
+++ 19-list.py
@@ -12,3 +12,14 @@ print(DevOps)
 # Adding the elements in the specific index using insert
 DevOps.insert(0, "ArgoCD")
 print(DevOps)
+
+# Removing the elements
+DevOps.remove("ArgoCD")
+print(DevOps)
+
+# Removing the elements at specific index or removing the last element
+DevOps.pop()
+print(DevOps)
+
+DevOps.pop(0)
+print(DevOps)
