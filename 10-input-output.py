0cc6228f0db2a733180a2ccc4d81b1e8e7fdd39e
Shankar-codes <harikainteriors@gmail.com>
2026-07-18 17:01:53 +0530

    python notes

---
+++ 10-input-output.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+
+# normal output
+print(f"The name is ",name, "age is ",age)
+# F string output
+print("f-string  - formatted string literals output")
+print(f"The name is {name} age is {age}")
