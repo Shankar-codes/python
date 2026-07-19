6b959b5a23e4069502ffbefb6e3f00422964768a
Shankar-codes <harikainteriors@gmail.com>
2026-07-19 12:30:26 +0530

    python notes

--- 24-sets-methos.py
+++ 24-sets-methos.py
@@ -3,3 +3,7 @@ DevOps = {"Linux", "Shell", "Ansible", "Terraform", "Docker", "Kubernates", "Lin
 # adding the elements
 DevOps.add("Jenkins")
 print(DevOps)
+
+# removing the elements
+DevOps.remove("Linux")
+print(DevOps)
