614e5620b55ec863236d789ed85399f478b92ab8
Shankar-codes <harikainteriors@gmail.com>
2026-07-19 15:53:17 +0530

    Python notes

--- 32-while-loop.py
+++ 32-while-loop.py
@@ -8,3 +8,12 @@ sheep_count=1
 while sheep_count<=10:
     print(f"Sheep {sheep_count}")
     sheep_count +=1
+    
+# break statement
+sheep_count = 1
+while sheep_count<=10:
+    print(f"Sheep {sheep_count}")
+    if sheep_count==5:
+        print("That's enough counting")
+        break
+    sheep_count +=1
