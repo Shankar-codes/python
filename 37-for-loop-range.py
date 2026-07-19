e849f2d1b1f369bd0547c8ac3e7f97e02e7f150b
Shankar-codes <harikainteriors@gmail.com>
2026-07-19 16:22:20 +0530

    Python notes

--- 37-for-loop-range.py
+++ 37-for-loop-range.py
@@ -1,2 +1,12 @@
 for i in range(1,11):
     print(i)
+
+print("\n")    
+print("printing even numbers")
+for i in range(2,12,2):
+    print(i)
+
+print("\n")
+print("printing odd numbers")
+for i in range(1, 12, 2):
+    print(i)
