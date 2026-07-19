b94733f62f8ef9af2ff059756ef66bc6c1d2db80
Shankar-codes <harikainteriors@gmail.com>
2026-07-19 12:54:22 +0530

    python notes

---
+++ 26-condition-statements.py
@@ -0,0 +1,11 @@
+# conditional statement - if
+time = 20
+if time == 20:
+    print("Greeting! Shankar, It's dinner time")
+
+# else condition
+time = 20
+if time == 18:
+    print("Greeting! Shankar, It's dinner time")
+else:
+    print("It's not dinner time yet")
