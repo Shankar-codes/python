3992520bd68366726b026e44faf9dde1d292882f
Shankar-codes <harikainteriors@gmail.com>
2026-07-19 12:14:22 +0530

    python notes

---
+++ 22-tuples-and-sets.py
@@ -0,0 +1,16 @@
+# Tuple
+DevOps = ("Linux", "Shell", "Ansible", "Terraform", "Docker", "AWS")
+print(DevOps)
+numbers=(1, 2, 3)
+# creating a tuple with one element:
+single_element_tuple=("Kubernetes",)
+
+# Accesing the tuple element
+print(DevOps[3])
+
+# Slicing the tuples
+print(DevOps[1:4])
+
+# Tuple concetation:
+merged_tuples=DevOps + numbers
+print(merged_tuples)
