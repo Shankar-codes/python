65de5b87c02ca7f6b838dee89dfd0a7418d1861b
Shankar-codes <harikainteriors@gmail.com>
2026-07-19 13:04:03 +0530

    python notes

---
+++ 27-voting-elegibility-checking.py
@@ -0,0 +1,6 @@
+age = int(input("Enter the age, it should be in numbers, not in strings"))
+
+if age >= 18 :
+    print(f"You can vote!")
+else:
+    print(f"your age is {age} and not eligible for voting")
