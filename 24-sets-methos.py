83075f04cae168c0c2ac0e0cca033da2b8bdfbe0
Shankar-codes <harikainteriors@gmail.com>
2026-07-19 12:36:38 +0530

    python notes

--- 24-sets-methos.py
+++ 24-sets-methos.py
@@ -6,4 +6,13 @@ print(DevOps)
 
 # removing the elements
 DevOps.remove("Linux")
+print("Removing the Linux element")
+print(DevOps)
+
+# discard the elements - CICD elements is not there but discard won't display the error
+DevOps.discard("CICD")
+print(DevOps)
+
+# removing the random element from the set
+DevOps.pop()
 print(DevOps)
