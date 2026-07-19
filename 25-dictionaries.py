a6462e202a65d7b818cb132e25c2f1540176f775
Shankar-codes <harikainteriors@gmail.com>
2026-07-19 12:43:02 +0530

    python notes

---
+++ 25-dictionaries.py
@@ -0,0 +1,14 @@
+DevOpswithEllamma = {
+    "Course": "DevOps",
+    "Trainer": "Shankar",
+    "Duration": "45 Hrs",
+    "Mode": "Online"
+}
+
+print(DevOpswithEllamma)
+
+# accessing the dictionary elements
+print(f"Course : {DevOpswithEllamma["Course"]}")
+print(f"Trainer : {DevOpswithEllamma["Trainer"]}")
+print(f"Duration : {DevOpswithEllamma["Duration"]}")
+print(f"Training Mode : {DevOpswithEllamma["Mode"]}")
