6a26bedc45c348d7480883a695ee043b26242944
Shankar-codes <harikainteriors@gmail.com>
2026-07-19 12:57:17 +0530

    python notes

--- 26-condition-statements.py
+++ 26-condition-statements.py
@@ -9,3 +9,15 @@ if time == 18:
     print("Greeting! Shankar, It's dinner time")
 else:
     print("It's not dinner time yet")
+    
+
+# elif statement
+time_1 = 15
+if time_1 == 18:
+    print("It's breakfast time")
+elif time_1 == 13:
+    print("It's lunch time")
+elif time_1 == 18:
+    print("It's dinner time")
+else:
+    print("It's not a meal time")
