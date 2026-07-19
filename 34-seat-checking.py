79b92fb7f776d9d4398342ed1040a2f3a760cb59
Shankar-codes <harikainteriors@gmail.com>
2026-07-19 16:06:15 +0530

    Python notes

---
+++ 34-seat-checking.py
@@ -0,0 +1,9 @@
+available_seat = 5
+while available_seat >= 0:
+    booking = input("Do you want to book the seat? (yes/no):").lower()
+    if booking=="yes":
+        available_seat-=1
+        print(f"Seat booked!. available seates: {available_seat} ")
+    else:
+        print(f"No booking made. available seates: {available_seat}")
+print("All seats are booked")
