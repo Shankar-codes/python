8cf03ad6bfef73492ef6bb3736a6d0dc9f72cc16
Shankar-codes <harikainteriors@gmail.com>
2026-07-20 18:45:03 +0530

    python notes

--- 48-functions.py
+++ 48-functions.py
@@ -2,3 +2,11 @@ def greetings():
     print("Hello Shankar! Have a nice day")
     
 greetings()
+
+
+# with prompt
+def greet_user(name):
+    print(f"Hello {name}! Have a nice day")
+    
+name=input("Enter the name: ")
+greet_user(name)
