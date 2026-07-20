f23e9f2e1874fc24384c2c1696465a2237ad19ec
Shankar-codes <harikainteriors@gmail.com>
2026-07-20 18:49:10 +0530

    python notes

--- 48-functions.py
+++ 48-functions.py
@@ -4,9 +4,16 @@ def greetings():
 greetings()
 
 
-# with prompt
+# function with parametes asking name with prompt
 def greet_user(name):
     print(f"Hello {name}! Have a nice day")
     
 name=input("Enter the name: ")
 greet_user(name)
+
+# returning values from a function
+def add_numbers(a,b):
+    return a + b
+
+result=add_numbers(10, 20)
+add_numbers
